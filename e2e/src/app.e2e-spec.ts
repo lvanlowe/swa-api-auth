d14cfd77e6d54b02c8866458dc3d9660e0916d61
lvanlowe <lvanlowe@inrvisions.com>
2024-07-27 06:57:28 -0400

    fix: correct app name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('swa-basic-api app is running!');
+    expect(page.getTitleText()).toEqual('swa-api-auth app is running!');
   });
 
   afterEach(async () => {
